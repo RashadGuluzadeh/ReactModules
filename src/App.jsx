0f02db72f21338b7836008ba6295a5b93afff3bc
RashadGuluzadeh <rashadquluzade@gmail.com>
2022-11-23 00:03:41 +0400

    task6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
+import SignUp from "./components/SignUp"
 
 function App() {
 
   return (
     <div className="App">
-     <h1>git checkout "branch name" </h1>
+     <SignUp />
     </div>
   )
 }
