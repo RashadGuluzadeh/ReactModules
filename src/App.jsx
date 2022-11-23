d0d0ce59e58b9715b5628819502d28949648df8d
RashadGuluzadeh <rashadquluzade@gmail.com>
2022-11-23 11:09:30 +0400

    task7

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
+import Form from './components/Form'
 
 function App() {
 
   return (
     <div className="App">
-     <h1>git checkout "branch name" </h1>
+     <Form />
     </div>
   )
 }
